0ac502ab3b2d219a66fd2a15b7df30fad1cd0b94
Lim <keilim@deloitte.com>
2018-10-16 15:51:35 -0400

    91 code

--- 91.decode-ways.java
+++ 91.decode-ways.java
@@ -11,37 +11,36 @@
  *
  * A message containing letters from A-Z is being encoded to numbers using the
  * following mapping:
- * 
- * 
+ *
+ *
  * 'A' -> 1
  * 'B' -> 2
  * ...
  * 'Z' -> 26
- * 
- * 
+ *
+ *
  * Given a non-empty string containing only digits, determine the total number
  * of ways to decode it.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: "12"
  * Output: 2
  * Explanation: It could be decoded as "AB" (1 2) or "L" (12).
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: "226"
  * Output: 3
  * Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2
  * 6).
- * 
+ *
  */
 class Solution {
     public int numDecodings(String s) {
-<<<<<<< HEAD
        return helper(s, 0);
     }
 
@@ -57,20 +56,17 @@ class Solution {
 	if (s.charAt(pos) == '0') return helper(s, pos + 1);
 	boolean getSecond = false;
 	if ( pos < s.length() -1 ) {
-	    
+
 	    if (s.charAt(pos) == '2') {
 	        if (s.charAt(pos+1) < '7') {
 		    getSecond = true;
 		}
 	    } else if (s.charAt(pos) < '2') {
-		getSecond = true;	
+		getSecond = true;
 	    }
 	}
 	return helper(s, pos + 1) +
 		((getSecond) ? helper(s, pos + 2) : 0);
-		
-=======
-        
->>>>>>> 58d0397d87381d6a7fc8960bac7255acb233bf3a
+
     }
 }
