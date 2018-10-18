56a1bcef8d428540e48ab309fdaf29458780b6ad
Lim <keilim@deloitte.com>
2018-10-18 10:02:47 -0400

    decode ways solved

--- 91.decode-ways.java
+++ 91.decode-ways.java
@@ -40,33 +40,66 @@
  *
  */
 class Solution {
-    public int numDecodings(String s) {
-        return helper(s, 0);
-    }
+	public int numDecodings(String s) {
+		if(s==null || s.length()==0 || s.charAt(0)=='0')
+			return 0;
+		if(s.length()==1)
+			return 1;
 
-    /**
-     * @param String s, int pos
-     * return int
-     * helper function that recurse through the string
-     * to find the total decoding
-     */
-    public int helper(String s, int pos) {
-	//base case
-	if (pos >= s.length()) return 1;
-	if (s.charAt(pos) == '0') return helper(s, pos + 1);
-	boolean getSecond = false;
-	if ( pos < s.length() -1 ) {
-
-	    if (s.charAt(pos) == '2') {
-	        if (s.charAt(pos+1) < '7') {
-		    getSecond = true;
+		int[] dp = new int[s.length()];    
+		dp[0]=1;
+		if(Integer.parseInt(s.substring(0,2))>26){
+			if(s.charAt(1)!='0'){
+				dp[1]=1;
+			}else{
+				dp[1]=0;
+			}
+		}else{
+			if(s.charAt(1)!='0'){
+				dp[1]=2;
+			}else{
+				dp[1]=1;
+			}
 		}
-	    } else if (s.charAt(pos) < '2') {
-		getSecond = true;
-	    }
-	}
-	return helper(s, pos + 1) +
-		((getSecond) ? helper(s, pos + 2) : 0);
 
-    }
+		for(int i=2; i<s.length(); i++){
+			if(s.charAt(i)!='0'){
+				dp[i]= dp[i] + dp[i-1];
+			}
+
+			int val = Integer.parseInt(s.substring(i-1, i+1));
+			if(val<=26 && val >=10){
+				dp[i]=dp[i] + dp[i-2];
+			}
+		}
+
+		return dp[s.length()-1];
+		//		return helper(s, 0, false);
+	}
+
+	/**
+	 * @param String s, int pos
+	 * return int
+	 * helper function that recurse through the string
+	 * to find the total decoding
+	 */
+	public int helper(String s, int pos, boolean number) {
+		//base case
+		if (pos >= s.length()) return 1;
+		if (s.charAt(pos) == '0') return 0;
+		boolean getSecond = false;
+		if ( pos < s.length() -1 ) {
+
+			if (s.charAt(pos) == '2') {
+				if (s.charAt(pos+1) < '7') {
+					getSecond = true;
+				}
+			} else if (s.charAt(pos) < '2') {
+				getSecond = true;
+			}
+		}
+		return helper(s, pos + 1, true) +
+			((getSecond) ? helper(s, pos + 2, true) : 0);
+
+	}
 }
