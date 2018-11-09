e6dc69589d4d6dc68ee7f0429f731b5f9c4698d9
Lim <keilim@deloitte.com>
2018-11-09 13:51:57 -0500

    Traversing pre and in my heart so much that it broke it, hard.

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -40,7 +40,52 @@
  * }
  */
 class Solution {
+    // Using example {10, 12, 3, 4, 6, 7, 5, 11, 2, 8}
+    // and  {3, 12, 6, 4, 7, 10, 11, 5, 2, 8}
+    // Create a hashmap<Integer, TreeNode>
+    // - go through pre order, and fill up root, then root.left.
+    // until the number of preorder is similar to first number in inorder. 
+    // F.E at 3
+    // - add it to Hashmap every time we add a new root.
+    // - go through in order until we find a number that is 
+    // not in the hashmap
+    // - we then take the number (inorderPos - 1) to get the root that
+    // we have to populate root.right with.
+    // - using the number, pull the root from the hashmap and 
+    // start working with that root.
+    // - populate right, but then populate left with preorder
+    // until it hits the same number. 
+    // - repeat. until end. 
+    // - ##IMportant, if the preOrderPos is at the end, then add it to the right. 
+    // - return HashMap.get(preOrder[0]);
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
+	if (preorder.length == 0 || preorder == null) return null;
+	if (preorder.length == 1) return new TreeNode(preorder[0]);
+        HashMap<Integer, TreeNode> hm = new HashMap<Integer, TreeNode>();
+	int inorderPos =0;
+	boolean found = false;
+	TreeNode root = new TreeNode(0);
+	TreeNode next;
+        for (int i = 0; i < preorder.length; i++) {
+		int currentVal = preorder[i];
+		next = new TreeNode(currentVal);
+		if (found) {
+			found = false;	
+			root.right = next;
+		} else if (root != null) {
+			root.left = next;
+		}
+		root = next;
+		hm.put(currentVal, next);
+		if (currentVal == inorder[inorderPos] && i < preorder.length) {
+			found = true;
+			while( inorderPos < inorder.length -1 && hm.containsKey(inorder[inorderPos])) {
+				inorderPos++;
+			}
+			System.out.println(inorder[inorderPos]);
+			root = hm.get(inorder[inorderPos - 1]);
+		}
+	}
+	return hm.get(preorder[0]);
     }
 }
