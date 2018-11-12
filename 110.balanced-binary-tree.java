0ac9f6e3a92bcb4e5a886b8228219a639ea916cc
Lim <keilim@deloitte.com>
2018-11-12 12:27:51 -0500

    have you ever tried balancing a see saw? It's like getting tree-d all over again

--- 110.balanced-binary-tree.java
+++ 110.balanced-binary-tree.java
@@ -58,7 +58,71 @@
  * }
  */
 class Solution {
+    // have a min and a max, whenever it reaches null, update the value
+    // get the difference of the min and max value, if it is more than one
+    // then return false.
+    // - helper {
+    // TreeNode root,
+    // int val(min/max)
+    // boolean isMax
+    // int depth
+    // }
+    //
+    // if isMax is true, then when we reach null. we'll compare it and make sure 
+    // max is max.
+    // helper {
+    // 	if null {
+    // 		if isMax return Math.max(depth, val);
+    // 		else return Math.min(depth, val);
+    // 	}
+    // 	depth++;
+    // 	if (isMax) return Math.max(helper(root.left, val, isMax, depth), 
+    // 					helper(root.right, val, isMax, depth));
+    // 	else return Math.min(helper(root.left, val, isMax, depth), 
+    // 					helper(root.right, val, isMax, depth));
+    // }
+    //
+    // solution {
+    // max = Math.max(helper(root.left, Integer.MIN_VALUE, true, 0), 
+    // 			helper(root.right, Integer.MIN_VALUE, true, 0);
+    // min = Math.min(helper(root.left, Integer.MAX_VALUE, false, 0), 
+    // 			helper(root.right, Integer.MAX_VALUE, false, 0);
+    //
+    // return (max - min <= 1);
+    // }
+
     public boolean isBalanced(TreeNode root) {
-        
+	if (root == null) return true;	
+//	int max = Math.max(helper(root.left, Integer.MIN_VALUE, true, 0),
+//			helper(root.right, Integer.MIN_VALUE, true, 0));
+//	int min = Math.min(helper(root.left, Integer.MAX_VALUE, false, 0),
+//			helper(root.right, Integer.MAX_VALUE, false, 0));
+//	System.out.println("min = " + min);
+//	System.out.println("max = " + max);
+//	return (max-min <= 1);
+	return height(root) != -1;
+    }
+
+    public int helper(TreeNode root, int val, boolean isMax, int depth) {
+	if (root == null) {
+		if (isMax) return Math.max(depth, val);
+		else return Math.min(depth, val);
+	}
+	depth++;
+	if (isMax) return Math.max(helper(root.left, val, isMax, depth), 
+			helper(root.right, val, isMax, depth));
+	else return Math.min(helper(root.left, val, isMax, depth),
+			helper(root.right, val, isMax, depth));
+
+    }
+
+    public int height(TreeNode node) {
+	    if (node ==null) return 0;
+	    int lh = height(node.left);
+	    if (lh == -1) return -1;
+	    int rh = height(node.right);
+	    if (rh == -1) return -1;
+	    if (lh-rh < -1 || lh - rh > 1) return -1;
+	    return Math.max(lh, rh)+1;
     }
 }
---
+++ 386.lexicographical-numbers.java
@@ -0,0 +1,29 @@
+/*
+ * [386] Lexicographical Numbers
+ *
+ * https://leetcode.com/problems/lexicographical-numbers/description/
+ *
+ * algorithms
+ * Medium (43.59%)
+ * Total Accepted:    34.1K
+ * Total Submissions: 78.3K
+ * Testcase Example:  '13'
+ *
+ * 
+ * Given an integer n, return 1 - n in lexicographical order.
+ * 
+ * 
+ * 
+ * For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+ * 
+ * 
+ * 
+ * Please optimize your algorithm to use less time and space. The input size
+ * may be as large as 5,000,000.
+ * 
+ */
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        
+    }
+}
