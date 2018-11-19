25ab64a73da38a06e7ab792d00bd71390428c23a
Lim <keilim@deloitte.com>
2018-11-19 13:43:04 -0500

    Can't believe numbers and tree are even freaking related.

--- 386.lexicographical-numbers.java
+++ 386.lexicographical-numbers.java
@@ -23,7 +23,35 @@
  * 
  */
 class Solution {
+    // technically, we can build it into a tree
+    // each tree starts with the first value from the front, then
+    // we can dfs and use preorder to get the value
+    // solution: {
+    // 	ArrayList<Integer> ls = new ArrayList<Integer>();
+    // 	for ( int i = 1; i < 10; i++) {
+    // 		dfs(i, ar, n);
+    // 	}
+    // 	return ar;
+    // }
+    //
+    //	dfs (int val, List<Integer> ar, int n) {
+    //		if (val <= n) ar.add(val);
+    //		for ( int i = 0; i < 10; i++) {
+    //			dfs(val * 10 + i, ar, n);
+    //		}
+    //	}
     public List<Integer> lexicalOrder(int n) {
-        
+	    ArrayList<Integer> ar = new ArrayList<Integer>();
+	    if (n < 1) return ar;
+	    for (int i = 1; i < 10; i++) dfs(i, ar,n);
+	    return ar;
+    }
+
+    public void dfs(int val, List<Integer> ar, int n) {
+	    if ( val <= n) ar.add(val);
+	    for (int i = 0; i < 10; i++) {
+		if (val * 10 + i > n) break;
+		dfs(val * 10 + i, ar, n);	
+	    }
     }
 }
