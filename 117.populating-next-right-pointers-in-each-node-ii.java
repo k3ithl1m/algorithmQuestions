6a096c5c1f1e42931185947fb8c73f97aab98030
Lim <keilim@deloitte.com>
2018-12-04 10:02:47 -0500

    this next pointer point this question to a week of time wasting

--- 117.populating-next-right-pointers-in-each-node-ii.java
+++ 117.populating-next-right-pointers-in-each-node-ii.java
@@ -72,6 +72,23 @@ public class Solution {
     // - we negate the level, and find the first leaf that is on the 
     // current level.	
     public void connect(TreeLinkNode root) {
-        
+       TreeLinkNode dummyHead = new TreeLinkNode(0);
+       TreeLinkNode pre = dummyHead;
+       while(root != null) {
+		if (root.left != null) {
+			pre.next = root.left;
+			pre = pre.next;
+		}
+		if (root.right != null) {
+			pre.next= root.right;
+			pre=pre.next;
+		}
+		root = root.next;
+		if (root == null) {
+			root = dummyHead.next;
+			pre = dummyHead;
+			dummyHead.next = null;
+		}
+       }
     }
 }
