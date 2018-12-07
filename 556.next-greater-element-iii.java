a0cec807c0f4b5f944e873b2382cdbf9e182509e
Keith Lim <keithlimweiluen@gmail.com>
2018-12-06 23:43:13 -0500

    pretty great

---
+++ 556.next-greater-element-iii.java
@@ -0,0 +1,69 @@
+/*
+ * [556] Next Greater Element III
+ *
+ * https://leetcode.com/problems/next-greater-element-iii/description/
+ *
+ * algorithms
+ * Medium (28.93%)
+ * Total Accepted:    20.7K
+ * Total Submissions: 70.5K
+ * Testcase Example:  '12'
+ *
+ * Given a positive 32-bit integer n, you need to find the smallest 32-bit
+ * integer which has exactly the same digits existing in the integer n and is
+ * greater in value than n. If no such positive 32-bit integer exists, you need
+ * to return -1.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 12
+ * Output: 21
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 21
+ * Output: -1
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int nextGreaterElement(int n) {
+	char[] car = String.valueOf(n).toCharArray();
+	int tracker = -1;
+	for (int i = car.length-1; i >= 1; i--) {
+		if (car[i-1] < car[i]) {
+			tracker = i-1;	
+			break;
+		}
+	}        
+
+	if (tracker >= 0) {
+		int temp = tracker + 1;
+		for (int i = tracker + 1; i < car.length; i++) {
+			if (car[temp] > car[i] && car[i] > car[tracker]) {
+				temp = i;
+			}	
+		}
+		char newtemp = car[tracker];
+		car[tracker] = car[temp];
+		car[temp] = newtemp;
+		Arrays.sort(car, tracker + 1, car.length);
+	} else {
+		return tracker;
+	}
+
+	try {
+		int result = Integer.parseInt(new String(car));
+		return result;
+	} catch(Exception e) {
+		return -1;
+	}
+    }
+}
