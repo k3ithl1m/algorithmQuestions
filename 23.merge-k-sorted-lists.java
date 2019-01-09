4bc288aba759a4ea0f5910141212260b3d03c428
keilim <keilim@deloitte.com>
2019-01-09 10:33:39 -0500

    hey

--- 23.merge-k-sorted-lists.java
+++ 23.merge-k-sorted-lists.java
@@ -35,47 +35,5 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        int i = 0;
-	while(lists.size() > 1) {
-	    if (i+1>l.size()) i=0;
-	    ListNode n = sort(lists.get(i), lists.get(i+1));
-	    lists.add(i,n);
-	    lists.remove(i+1);
-	    lists.remove(i+2);
-	    i+=2;
-	}
-	return lists.get(0);
-    }
-
-    public ListNode sort(ListNode a, ListNode b) {
-	ListNode head;
-	if (a.val > b.val) {
-	    head = b;
-	    b=b.next;
-	} else {
-	    head = a;
-	    a=a.next;
-	}
-	ListNode pt = head;
-	while (b!=null ||a!=null) {
-	    if (a==null) {
-		pt.next=b;
-		break;
-	    }
-	    if(b==null) {
-		pt.next=a;
-		break;
-	    }
-	    if(a.val > b.val) {
-		pt.next = b;
-		b=b.next;
-		pt=pt.next;
-	    } else {
-		pt.next = a;
-		a=a.next;
-		pt=pt.next;
-	    }
-	}
-	return head;
     }
 }
