ef171df4d918178ef4cb2806b66d3bc479b43b80
keilim <keilim@deloitte.com>
2019-01-09 10:34:51 -0500

    s

--- 85.maximal-rectangle.java
+++ 85.maximal-rectangle.java
@@ -1,29 +1,20 @@
 /*
-<<<<<<< HEAD
- * @lc app=leetcode id=85 lang=java
- *
-=======
->>>>>>> cfff3562fb6c04503cc451885b1841db0a27e4a3
  * [85] Maximal Rectangle
  *
  * https://leetcode.com/problems/maximal-rectangle/description/
  *
  * algorithms
-<<<<<<< HEAD
- * Hard (31.60%)
-=======
  * Hard (31.54%)
->>>>>>> cfff3562fb6c04503cc451885b1841db0a27e4a3
  * Total Accepted:    104.8K
  * Total Submissions: 331.2K
  * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
  *
  * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle
  * containing only 1's and return its area.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input:
  * [
  * ⁠ ["1","0","1","0","0"],
@@ -32,11 +23,11 @@
  * ⁠ ["1","0","0","1","0"]
  * ]
  * Output: 6
- * 
- * 
+ *
+ *
  */
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        
+
     }
 }
