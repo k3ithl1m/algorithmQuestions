aac7cb847165a116d515012b10fbb7e1b72ba4c1
keilim <keilim@deloitte.com>
2019-01-14 12:51:32 -0500

    update 463.island-perimeter.java

--- 463.island-perimeter.java
+++ 463.island-perimeter.java
@@ -44,13 +44,15 @@
  */
 class Solution {
     public int islandPerimeter(int[][] grid) {
-	boolean[][] visitedIsland = new boolean[grid.length][grid[0].length];
     	int totalPerimeter = 0;
 	for (int i = 0; i < grid.length; i++) {
 		for (int j = 0; j<grid[0].length; j++) {
 			if (grid[i][j] == 1) {
-				totalPerimeter = dfs(grid, visitedIsland, i, j, totalPerimeter);
-				break;
+				totalPerimeter += 4;
+				if (i-1 >= 0 && grid[i-1][j] == 1) totalPerimeter--;
+				if (i+1 < grid.length && grid[i+1][j] == 1) totalPerimeter--;
+				if (j-1 >= 0 && grid[i][j-1] == 1) totalPerimeter--;
+				if (j+1 < grid[0].length && grid[i][j+1] == 1) totalPerimeter--;
 			}
 		}
 	}        
