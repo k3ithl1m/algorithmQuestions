9f378368beb17d416592231e62376a950872f92e
keilim <keilim@deloitte.com>
2019-01-24 19:54:49 -0500

    update 647.palindromic-substrings.java

--- 647.palindromic-substrings.java
+++ 647.palindromic-substrings.java
@@ -43,6 +43,18 @@
  */
 class Solution {
     public int countSubstrings(String s) {
-        
+	if (s.length() == 0 || s == null) return 0;
+	int count = 0;
+	for (int i = 0; i < s.length(); i++) {
+		count+= expandCenter(s, i,i);
+		count+= expandCenter(s, i, i+1);
+	}         
+	return count;
     }
+
+	public int expandCenter(String s, int left, int right) {
+		int count = 0;
+		while(left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) count++;
+		return count;
+	}
 }
