4d9504b88d2f5665c6fab7354be7c624a949b6d2
keilim <keilim@deloitte.com>
2019-01-30 18:29:53 -0500

    coin chagned

--- 322.coin-change.java
+++ 322.coin-change.java
@@ -12,8 +12,7 @@
  * Testcase Example:  '[1,2,5]\n11'
  *
  * You are given coins of different denominations and a total amount of money
- * amount. Write a function to compute the fewest number of coins that you need
- * to make up that amount. If that amount of money cannot be made up by any
+ * amount. Write a function to compute the fewest number of coins that you need * to make up that amount. If that amount of money cannot be made up by any
  * combination of the coins, return -1.
  * 
  * Example 1:
@@ -35,7 +34,52 @@
  * 
  */
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        
+    public int coinChange2(int[] coins, int amount) {
+	if (coins.length == 0) return -1;
+	int val = recurseCoinCount(coins, amount,new int[amount+1]);
+	return (val == Integer.MAX_VALUE) ? -1 : val;
     }
+
+    public int coinChange(int[] coins, int amount) {
+	if (coins.length == 0) return -1;
+	int[] dp = new int[amount + 1];
+	Arrays.fill(dp, amount+1);
+	for (int i = 1; i <= amount; i++) {
+		for (int coin : coins) {
+			if (coin <= i) {
+				dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+			}
+		}	
+	}
+	return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    private int coinChange(int[] coins, int rem, int[] count) {
+	if (rem < 0) return -1;
+	if (rem == 0) return 0;
+	if (count[rem - 1] != 0) return count[rem-1];
+	int min = Integer.MAX_VALUE;
+	for (int coin: coins) {
+		int res = coinChange(coins, rem - coin, count);
+		if (res >= 0 && res < min) min = 1 + res;
+	}
+	count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+	return count[rem - 1];
+    }
+
+    public int recurseCoinCount(int[] coins, int amount, int[] coinCache) {
+	if (amount < 0) return Integer.MAX_VALUE;
+	if (amount == 0) return 0;
+	if (coinCache[amount] != 0) return coinCache[amount];
+	int currentMin = Integer.MAX_VALUE;
+	for (int coin: coins) {
+		if (amount - coin >= 0) {
+			int nowMin = recurseCoinCount(coins, amount- coin, coinCache);	
+			if (nowMin >= 0 && nowMin < currentMin) currentMin = nowMin + 1;
+		}
+	}
+	coinCache[amount] = currentMin;
+	return coinCache[amount];
+    }
+
 }
---
+++ 509.fibonacci-number.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=509 lang=java
+ *
+ * [509] Fibonacci Number
+ *
+ * https://leetcode.com/problems/fibonacci-number/description/
+ *
+ * algorithms
+ * Easy (66.37%)
+ * Total Accepted:    10.7K
+ * Total Submissions: 16.2K
+ * Testcase Example:  '2'
+ *
+ * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the
+ * Fibonacci sequence, such that each number is the sum of the two preceding
+ * ones, starting from 0 and 1. That is,
+ * 
+ * 
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), for N > 1.
+ * 
+ * 
+ * Given N, calculate F(N).
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2
+ * Output: 1
+ * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 3
+ * Output: 2
+ * Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 4
+ * Output: 3
+ * Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 0 ≤ N ≤ 30.
+ * 
+ */
+class Solution {
+    public int fib(int N) {
+	if (N <= 1) return N;        
+	int prev = 0;
+	int next = 1;
+	for (int i = 2; i <= N; i++) {
+		int temp = prev + next;
+		prev = next;
+		next = temp;
+	}
+	return next;
+    }
+
+    public int recurse(int N, int[] cache) {
+	if (N <= 1) return N;
+	if (cache[N] != 0) return cache[N];
+	cache[N] = recurse(N-1, cache) + recurse(N-2, cache);
+	return cache[N];	
+    }
+}
