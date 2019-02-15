425a2cbd79772982e5706a5862ff894688878d72
Keith Lim <keithlimweiluen@gmail.com>
2019-02-14 22:56:06 -0500

    tried and failed

--- 652.find-duplicate-subtrees.java
+++ 652.find-duplicate-subtrees.java
@@ -56,6 +56,47 @@
  */
 class Solution {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        
+	if (root == null) return new ArrayList<TreeNode>();
+	HashMap<Integer, ArrayList<TreeNode>> valToArrayOfTreeNodeMap = 
+			new HashMap<Integer, ArrayList<TreeNode>>();
+
+	fillMapWithNodes(root, valToArrayOfTreeNodeMap);
+	ArrayList<TreeNode> result = new ArrayList<TreeNode>();
+	for (int key : valToArrayOfTreeNodeMap.keySet()) {
+		ArrayList<TreeNode> tempArray = valToArrayOfTreeNodeMap.get(key);
+		for (int i = 0; i < tempArray.size(); i++) {
+			for (int j = i + 1; j < tempArray.size(); j++) {
+				if (!result.contains(tempArray.get(i)) && 
+					checkEqual(tempArray.get(i), tempArray.get(j))) 
+				{
+					for (int k = 0; k < result.size(); k++) {
+						if (checkEqual(result.get(k), tempArray.get(i))) {
+							result.add(tempArray.get(i));
+						}
+					}
+				}
+			}
+		}
+	} 
+
+	return result;
+    }
+
+    private void fillMapWithNodes(TreeNode root, HashMap<Integer, ArrayList<TreeNode>> map) {
+	if (root == null) return;
+	ArrayList<TreeNode> treeNodeArray = map.get(root.val);
+	if (treeNodeArray == null) {
+		treeNodeArray = new ArrayList<TreeNode>();
+		treeNodeArray.add(root);
+		map.put(root.val, treeNodeArray);
+	} else treeNodeArray.add(root);
+	fillMapWithNodes(root.left, map);
+	fillMapWithNodes(root.right, map);
+    }
+
+    private boolean checkEqual(TreeNode left, TreeNode right) {
+	if (left == null && right == null) return true;
+	if (left == null || right == null) return false;
+	return left.val == right.val && checkEqual(left.left, right.left) && checkEqual(left.right, right.right);
     }
 }
