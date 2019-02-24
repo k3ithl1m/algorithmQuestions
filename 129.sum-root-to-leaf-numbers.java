bac4a51e0b721732755c66b3484f0f8c239af1a3
Keith Lim <keithlimweiluen@gmail.com>
2019-02-23 20:45:10 -0500

    update 129.sum-root-to-leaf-numbers.java

---
+++ 129.sum-root-to-leaf-numbers.java
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=129 lang=java
+ *
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (41.13%)
+ * Total Accepted:    170.4K
+ * Total Submissions: 413.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ * 
+ * An example is the root-to-leaf path 1->2->3 which represents the number
+ * 123.
+ * 
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+	if (root == null) return 0;
+	int[] sumStore = new int[]{0};
+	traverseSum(root, 0, sumStore);
+	return sumStore[0];        
+    }
+
+    private void traverseSum(TreeNode root, int prevAmount, int[] sumStore) {
+	if (root == null) return;
+	int total = prevAmount * 10 + root.val;
+	if (root.left == null && root.right == null) sumStore[0] += total;
+	else if (root.left == null) traverseSum(root.right, total, sumStore);
+	else if (root.right == null) traverseSum(root.left, total, sumStore);
+	else {
+		traverseSum(root.right, total, sumStore);
+		traverseSum(root.left, total, sumStore);
+	}
+    }
+}
