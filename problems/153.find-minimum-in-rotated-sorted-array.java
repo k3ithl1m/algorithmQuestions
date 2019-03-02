0f478914f007a4e898c226a71408004b99a1f2d1
Keith Lim <keithlimweiluen@gmail.com>
2019-03-02 16:44:31 -0500

    envelopes

--- problems/153.find-minimum-in-rotated-sorted-array.java
+++ problems/153.find-minimum-in-rotated-sorted-array.java
@@ -37,6 +37,50 @@ class Solution {
     public int findMin(int[] nums) {
 	if (nums.length == 0) return 0;
 	if (nums.length == 1) return nums[0];
+	if (nums.length == 2) return (nums[1] > nums[0]) ? nums[0] : nums[1];
+	int left = 0, right = nums.length - 1;
+	while (left < right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] > nums[right]) left = mid+1;
+		else if (nums[mid] < nums[right]) right = mid;
+		else --right;
+	}
+
+	return nums[left];
+    }
+
+
+    public int findMin3(int[] nums) {
+	if (nums.length == 0) return 0;
+	if (nums.length == 1) return nums[0];
+	if (nums.length == 2) return (nums[1] > nums[0]) ? nums[0] : nums[1];
+	int left = 0, right = nums.length - 1;
+	while (left <= right) {
+		int mid = left + (right-left) /2;
+		if (mid - 1 >= 0 && nums[mid-1] > nums[mid]) return nums[mid];
+		else if (mid - 1 < 0) return nums[mid];
+
+		if (nums[mid] > nums[left]) {
+			if (nums[mid] <= nums[right]) right = mid;
+			else left = mid;
+		} else if (nums[mid] < nums[left]) {
+			if (nums[mid] > nums[right]) left = mid;
+			right = mid;
+		} else left++;
+	}
+	return 0;
+    }
+
+
+
+
+
+
+
+
+    public int findMin2(int[] nums) {
+	if (nums.length == 0) return 0;
+	if (nums.length == 1) return nums[0];
 	int left = nums[0], right = nums[nums.length - 1];
 	if (left < right) return left;
 	int leftPointer = 0, rightPointer = nums.length;
