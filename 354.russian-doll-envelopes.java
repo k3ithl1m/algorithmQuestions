aa42382f519ec8aee273a086f6fabf6216261f54
Keith Lim <keithlimweiluen@gmail.com>
2019-03-02 16:47:19 -0500

    update 354.russian-doll-envelopes.java

--- 354.russian-doll-envelopes.java
+++ 354.russian-doll-envelopes.java
@@ -111,7 +111,7 @@ class Solution {
 	Arrays.sort(envelopes, new Comparator<int[]>() {
 		@Override
 		public int compare(int[] a, int[] b) {
-			if (a[0] == b[0]) return a[1] - b[1];
+			if (a[0] == b[0]) return b[1] - a[1];
 			else return a[0] - b[0];
 		}
 	});       
